5b4517c5cd58cfdd6da9356bbd59c1debe8aea0a
Mikhail Golubitsky <mike617@gmail.com>
2022-12-04 00:56:33 -0500

    finish day 3

--- day_03.rb
+++ day_03.rb
@@ -1,23 +1,28 @@
 module RucksackParser
   extend self
 
-  def rucksacks(input_filepath)
+  def part_one_rucksacks(input_filepath)
     File.readlines(input_filepath)
         .map(&:strip)
-        .map { |row| rucksack(row) }
+        .map { |row| part_one_rucksack(row) }
+  end
+
+  def part_two_rucksacks(input_filepath)
+    File.readlines(input_filepath)
+        .map(&:strip)
+        .map(&:chars)
+        .each_slice(3)
+        .to_a
   end
 
   private
 
-  def rucksack(row)
+  def part_one_rucksack(row)
     midpoint_index = row.size / 2
     first_half = row[0...midpoint_index].chars
     second_half = row[midpoint_index..-1].chars
 
-    {
-      compartment: first_half,
-      other_compartment: second_half
-    }
+    [first_half, second_half]
   end
 end
 
@@ -33,28 +38,34 @@ module SumOfPriorities
 
   extend self
 
-  def sum_of_priorities(rucksacks)
-    rucksacks.map { |rucksack| priority(rucksack) }
-             .sum
+  # Accepts an array of either rucksack compartments or of multiple rucksacks
+  def sum_of_priorities(collection_of_compartments)
+    collection_of_compartments.map { |compartments| priority(compartments) }
+                              .sum
   end
 
   private
 
-  def priority(rucksack)
-    PRIORITY_BY_ITEM[item_that_appears_in_both_compartments(rucksack)]
+  def priority(compartments)
+    PRIORITY_BY_ITEM[common_item(compartments)]
   end
 
-  def item_that_appears_in_both_compartments(rucksack)
-    intersection(rucksack[:compartment], rucksack[:other_compartment]).first
+  def common_item(compartments)
+    intersection(*compartments).first
   end
 
-  def intersection(array, other_array)
-    array & other_array
+  def intersection(*arrays)
+    arrays.inject { |intersection, array| intersection & array }
   end
 end
 
 if __FILE__ == $PROGRAM_NAME
   input_filepath = 'data/day_03.txt'
-  rucksacks = RucksackParser.rucksacks(input_filepath)
-  pp SumOfPriorities.sum_of_priorities(rucksacks)
+  pp SumOfPriorities.sum_of_priorities(
+    RucksackParser.part_one_rucksacks(input_filepath)
+  )
+
+  pp SumOfPriorities.sum_of_priorities(
+    RucksackParser.part_two_rucksacks(input_filepath)
+  )
 end
