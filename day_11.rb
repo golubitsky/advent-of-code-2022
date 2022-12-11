898f30bc2ed727fad11e320b63fea7dc5f0219e1
Mikhail Golubitsky <mike617@gmail.com>
2022-12-11 02:44:20 -0500

    complete ruby day 11 part 2 with Reddit hints

--- day_11.rb
+++ day_11.rb
@@ -24,7 +24,7 @@ module Parser
       yes: five_lines[4].scan(/\d+/).first.to_i,
       no: five_lines[5].scan(/\d+/).first.to_i
     }
-    {
+    m = {
       starting_items: starting_items,
       operation: operation,
       test: test
@@ -39,22 +39,39 @@ end
 module Solution
   extend self
 
-  def solution(monkeys)
+  def solution(monkeys, reduce_worry_levels_during_round:, rounds:)
     monkey_inspection_counts = Hash.new(0)
-    20.times { perform_round(monkeys, monkey_inspection_counts) }
-    level_of_monkey_business = monkey_inspection_counts.sort_by do |_index, count|
-      -count
-    end.to_h.values.take(2).reduce(:*)
+
+    rounds.times do
+      perform_round(monkeys, monkey_inspection_counts, reduce_worry_levels_during_round)
+    end
+
+    monkey_inspection_counts.sort_by { |_index, count| -count }
+                            .to_h
+                            .values
+                            .take(2)
+                            .reduce(:*)
   end
 
   private
 
-  def perform_round(monkeys, monkey_inspection_counts)
+  def perform_round(monkeys, monkey_inspection_counts, reduce_worry_levels_during_round)
+    # I don't get this number theory thing. Cheated and read about it on Reddit.
+    product_divisors = monkeys.map { |x| x[:test][:divisible_by] }.reduce(:*)
+
     monkeys.each_with_index do |monkey, index|
       monkey_inspection_counts[index] += monkey[:starting_items].count
 
-      multiplied = monkey[:starting_items].map { |item| apply_operation(item, monkey[:operation]) }
-      bored = multiplied.map { |item| item / 3 }
+      operation_applied = monkey[:starting_items].map { |item| apply_operation(item, monkey[:operation]) }
+
+      bored = if reduce_worry_levels_during_round
+                operation_applied.map { |item| item / 3 }
+              else
+                (operation_applied.map do |item|
+                   item % product_divisors
+                 end)
+              end
+
       result_monkeys_indexes = bored.map do |bored_item|
         if (bored_item % monkey.dig(:test, :divisible_by)).zero?
           monkey.dig(:test, :yes)
@@ -89,6 +106,8 @@ end
 
 if __FILE__ == $PROGRAM_NAME
   monkeys = Parser.parse('data/day_11.txt')
+  pp Solution.solution(monkeys, reduce_worry_levels_during_round: true, rounds: 20)
 
-  pp Solution.solution(monkeys)
+  monkeys = Parser.parse('data/day_11.txt')
+  pp Solution.solution(monkeys, reduce_worry_levels_during_round: false, rounds: 10_000)
 end
