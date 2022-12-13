5e3cf24ab994765f83fe3cebe786f08660c5f44c
Mikhail Golubitsky <mike617@gmail.com>
2022-12-13 01:23:19 -0500

    refactor so the sort function can be used without minusing it

--- day_13.rb
+++ day_13.rb
@@ -8,14 +8,6 @@ module Parser
         .map(&:strip)
         .reject { |line| line == '' }
         .map { |line| eval(line) }
-        .each_slice(2)
-  end
-
-  def parse_part_two(filepath)
-    File.readlines(filepath)
-        .map(&:strip)
-        .reject { |line| line == '' }
-        .map { |line| eval(line) }
   end
 end
 
@@ -25,20 +17,20 @@ module Solution
   def solution(input)
     indexes_in_right_order = []
 
-    input.each_with_index do |(left, right), index|
-      indexes_in_right_order << index + 1 if lists_in_right_order?(left, right) == 1
+    input.each_slice(2).each_with_index do |(left, right), index|
+      indexes_in_right_order << index + 1 if lists_in_right_order?(left, right) == -1
     end
 
     indexes_in_right_order.sum
   end
 
-  def sort_with_divider_packets(input)
+  def decoder_key_from_sorted_packets(input)
     divider_packet = [[2]]
     other_divider_packet = [[6]]
 
     packets = input + [divider_packet, other_divider_packet]
 
-    sorted = packets.sort { |a, b| -lists_in_right_order?(a, b) }
+    sorted = packets.sort { |a, b| lists_in_right_order?(a, b) }
 
     (sorted.index(divider_packet) + 1) * (sorted.index(other_divider_packet) + 1)
   end
@@ -59,7 +51,7 @@ module Solution
             next
           end
 
-          return left < right ? 1 : -1
+          return left < right ? -1 : 1
         elsif list_and_list?(left, right)
           inner = lists_in_right_order?(left, right)
           if inner == 0
@@ -89,9 +81,9 @@ module Solution
           raise "unknown data #{left} #{right}"
         end
       elsif right
-        return 1
-      elsif left
         return -1
+      elsif left
+        return 1
       else
         raise 'unexpected condition'
       end
@@ -117,26 +109,8 @@ module Solution
   end
 end
 
-# If both values are integers, the lower integer should come first.
-# If the left integer is lower than the right integer, the inputs are in the right order.
-# If the left integer is higher than the right integer, the inputs are not in the right order.
-# Otherwise, the inputs are the same integer; continue checking the next part of the input.
-
-# If both values are lists, compare the first value of each list,
-# then the second value, and so on. If the left list runs out of items first,
-# the inputs are in the right order. If the right list runs out of items first,
-# the inputs are not in the right order. If the lists are the same length and
-# no comparison makes a decision about the order, continue checking the next part of the input.
-
-# If exactly one value is an integer, convert the integer to a list which contains
-# that integer as its only value, then retry the comparison.
-# For example, if comparing [0,0,0] and 2, convert the right value to [2]
-# (a list containing 2); the result is then found by instead comparing [0,0,0] and [2].
-
 if __FILE__ == $PROGRAM_NAME
-  path = 'data/day_13.txt'
-  parsed = Parser.parse(path)
+  parsed = Parser.parse('data/day_13.txt')
   pp Solution.solution(parsed)
-  parsed = Parser.parse_part_two(path)
-  pp Solution.sort_with_divider_packets(parsed)
+  pp Solution.decoder_key_from_sorted_packets(parsed)
 end
