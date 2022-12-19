9a9b068297e4b22723b9ea3860428fa667a0b24c
Mikhail Golubitsky <mike617@gmail.com>
2022-12-18 23:52:26 -0500

    make a mess out of day 18 part 2; unsolved

--- day_18.rb
+++ day_18.rb
@@ -22,33 +22,94 @@ module Solution
     end
   end
 
-  def part_two(input)
+  def part_two(cubes, pockets)
+    all_cubes = cubes.to_set
+    all_pockets = pockets.to_set
+
+    surface_area = cubes.sum do |(x, y, z)|
+      adjacent_cubes(x, y, z).count do |cube|
+        !all_cubes.include?(cube) && !all_pockets.include?(cube)
+      end
+    end
+  end
+
+  def part_two_precompute_pockets(input, path_to_write_pockets)
     all_cubes = input.to_set
 
-    surface_area = input.sum do |(x, y, z)|
-      count_exposed_surface_area(x, y, z, all_cubes)
-    end
-
-    p xs = input.map { |(x, _y, _z)| x }.minmax
-    p ys = input.map { |(_x, y, _z)| y }.minmax
-    p zs = input.map { |(_x, _y, z)| z }.minmax
-    (xs.first..xs.last).each do |x|
-      (ys.first..ys.last).each do |y|
-        (zs.first..zs.last).each do |z|
+    p x_limits = input.map { |(x, _y, _z)| x }.minmax
+    p y_limits = input.map { |(_x, y, _z)| y }.minmax
+    p z_limits = input.map { |(_x, _y, z)| z }.minmax
+    pockets = Set.new
+    (x_limits.first..x_limits.last).each do |x|
+      p "checking x=#{x}"
+      (y_limits.first..y_limits.last).each do |y|
+        (z_limits.first..z_limits.last).each do |z|
           cur_cube = [x, y, z]
           next if all_cubes.include?(cur_cube)
 
           # TODO: this only finds a pocket of one cube; so the surface_area ends
           # up too high. Need to recursively look for larger pockets...
-          surface_area -= 6 if adjacent_cubes(x, y, z).all? { |cube| all_cubes.include?(cube) }
+          path = bfs(cur_cube, all_cubes, x_limits, y_limits, z_limits, pockets)
+          next unless path
+
+          p 'discovered pocket'
+          open(path_to_write_pockets, 'a') do |f|
+            path.each { |cube| f.puts cube.join(',') }
+          end
         end
       end
     end
-
-    surface_area
   end
 
   private
+
+  def bfs(start_cube, all_cubes, x_limits, y_limits, z_limits, pockets)
+    q = []
+    explored = Set.new
+    parents = {}
+
+    # only start searching if it's not a cube
+    return false if all_cubes.include?(start_cube)
+    # already counted these
+    return false if pockets.include?(start_cube)
+
+    explored.add(start_cube)
+
+    q.push(start_cube)
+
+    while q.any?
+      cube = q.shift
+      x, y, z = cube
+
+      # if can get to edge from a non-cube, not in a pocket
+      return false if (x < x_limits[0] || x > x_limits[1]) &&
+                      (y < y_limits[0] || y > y_limits[1]) &&
+                      (z < z_limits[0] || z > z_limits[1])
+
+      adjacent_cubes(x, y, z).each do |adj_cube|
+        next if all_cubes.include?(adj_cube)
+        next if explored.include?(adj_cube)
+
+        explored.add(adj_cube)
+        parents[adj_cube] = cube
+        q.push(adj_cube)
+
+        return trace_path_back(from: adj_cube, to: start_cube, parents: parents) if pockets.include?(adj_cube)
+      end
+    end
+
+    trace_path_back(from: cube, to: start_cube, parents: parents)
+  end
+
+  def trace_path_back(from:, to:, parents:)
+    cur = from
+    path = []
+    while cur != to
+      cur = parents[cur]
+      path << cur
+    end
+    path
+  end
 
   def count_exposed_surface_area(x, y, z, all_cubes)
     adjacent_cubes(x, y, z).count { |cube| !all_cubes.include?(cube) }
@@ -67,7 +128,10 @@ module Solution
 end
 
 if __FILE__ == $PROGRAM_NAME
-  parsed = Parser.parse('data/day_18.txt')
-  pp Solution.part_one(parsed)
-  pp Solution.part_two(parsed)
+  cubes = Parser.parse('data/day_18.txt')
+  pp Solution.part_one(cubes)
+  pp Solution.part_two_precompute_pockets(cubes, 'data/day_18_pockets_sample.txt')
+
+  pockets = Parser.parse('data/day_18_pockets.txt').uniq
+  pp Solution.part_two(cubes, pockets)
 end
