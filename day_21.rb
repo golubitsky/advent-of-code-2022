7d974f407a690fea9f380f0c6af5665fee90606f
Mikhail Golubitsky <mike617@gmail.com>
2022-12-21 01:29:23 -0500

    solve 21:2 with manual assistance

--- day_21.rb
+++ day_21.rb
@@ -44,9 +44,39 @@ module Solution
     end
     monkeys[:root][:result]
   end
+
+  def part_two(monkeys, humn_value:)
+    monkeys[:humn][:result] = humn_value
+
+    until monkeys[:root][:result]
+      monkeys.each do |name, operation|
+        next if operation[:result]
+
+        a = monkeys[operation[:a]][:result]
+        b = monkeys[operation[:b]][:result]
+
+        next unless a && b
+
+        monkeys[name][:result] = a.send(operation[:operand], b)
+      end
+    end
+    root = monkeys[:root]
+    # require 'pry'; binding.pry
+    # puts monkeys[root[:b]][:result]
+    [
+      monkeys[root[:a]][:result],
+      monkeys[root[:b]][:result]
+    ]
+  end
 end
 
 if __FILE__ == $PROGRAM_NAME
   parsed = Parser.parse('data/day_21.txt')
   pp Solution.part_one(parsed)
+
+  i = 3_665_520_865_940
+  parsed = Parser.parse('data/day_21.txt')
+  result = Solution.part_two(parsed, humn_value: i)
+  p result.map(&:to_s).map(&:length)
+  pp "#{result} diff=#{result.last - result.first} i=#{i}"
 end
