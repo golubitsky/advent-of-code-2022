ae960baaf07bd20bb15d90f50b20571d8948670b
Mikhail Golubitsky <mike617@gmail.com>
2022-12-25 17:45:44 -0500

    day 16: on the right track, but wrong answer

--- day_16.rb
+++ day_16.rb
@@ -1,5 +1,7 @@
 # frozen_string_literal: true
 
+require 'set'
+
 module Parser
   extend self
 
@@ -8,8 +10,7 @@ module Parser
         .each_with_object({}) do |line, result|
           result[line.split[1].strip.to_sym] = {
             rate: line.scan(/\d+/).first.to_i,
-            tunnels: line.split(/[valves]|[valve]/).last.split(',').map(&:strip).map(&:to_sym),
-            opened_during_minute: nil
+            tunnels: line.split(/[vales]|[vale]/).last.split(',').map(&:strip).map(&:to_sym)
           }
         end
   end
@@ -18,66 +19,97 @@ end
 module Solution
   extend self
 
-  RESULT = []
-  $explored_count = 0
-  def solution(state)
-    backtracking_search(minute: 1, valve_name: state.keys.first, state: state)
-    RESULT.map { |state| count_pressure_released(state) }.sort.last(5)
+  MINUTES = 30
+
+  def solution(graph)
+    best = backtracking_search(
+      graph: graph,
+      node: graph.keys.first,
+      visited: Set.new,
+      memo: Set.new,
+      minutes_left: MINUTES,
+      minute_opened_by_valve: graph.transform_values { |_| nil }
+    )
+
+    count_pressure_released(graph, best)
   end
 
   private
 
-  def backtracking_search(minute:, valve_name:, state:)
-    $explored_count += 1
-    p $explored_count if $explored_count % 1_000_000 == 0
-    if minute == 31
-      RESULT << state
-      return
-    end
-    if all_valves_with_flow_open?(state)
-      p state
-      p count_pressure_released(state)
-      RESULT << state
-      return
-    end
+  # rubocop:disable Metrics/MethodLength
+  # rubocop:disable Metrics/AbcSize
+  # rubocop:disable Metrics/CyclomaticComplexity
+  def backtracking_search(graph:, node:, visited:, memo:, minutes_left:, minute_opened_by_valve:)
+    # puts "\n#{node} min left: #{minutes_left}"
+    # p minute_opened_by_valve
+    return minute_opened_by_valve if
+      memo.include?([node, minute_opened_by_valve]) ||
+      minutes_left.zero? ||
+      all_valves_with_flow_open?(graph, minute_opened_by_valve)
 
-    # TODO: need better heuristic to determine which edge to go to first.
-    # TODO: detect cycles? e.g. makes no sense to keep hopping back and forth
-    # between :II and :JJ ad infinitum. And there are larger cycles. Cycles
-    # of valves already open.
-    unless state[valve_name][:opened_during_minute] # nothing else to explore
-      backtracking_search(
-        minute: minute + 1,
-        valve_name: valve_name, # stay here, spend minute to open valve
-        state: state.merge(
-          valve_name => state[valve_name].merge(opened_during_minute: minute)
-        )
+    memo.add([node, minute_opened_by_valve])
+
+    best_minute_opened_by_valve = minute_opened_by_valve
+    best = count_pressure_released(graph, minute_opened_by_valve)
+
+    unless minute_opened_by_valve[node] # nothing else to explore
+      # p "opening #{node}"
+      # spend minute to open valve
+      inner = backtracking_search(
+        graph: graph,
+        node: node,
+        visited: visited,
+        memo: memo,
+        minutes_left: minutes_left - 1,
+        minute_opened_by_valve: minute_opened_by_valve.merge(node => minutes_left)
       )
+      cur = count_pressure_released(graph, inner)
+      # puts "emerged: #{cur}"
+
+      if cur > best
+        best_minute_opened_by_valve = inner
+        best = cur
+      end
     end
 
-    # explore the highest rate valves first
-    sorted = state[valve_name][:tunnels].sort_by { |to| -state[to][:rate] }
-    sorted.each do |to_valve|
+    graph[node][:tunnels].each do |other_node|
+      # p "going to #{other_node}"
       # spend minute to go to another valve
-      backtracking_search(minute: minute + 1, valve_name: to_valve, state: state)
+      inner = backtracking_search(
+        graph: graph,
+        node: other_node,
+        visited: visited,
+        memo: memo,
+        minutes_left: minutes_left - 1,
+        minute_opened_by_valve: minute_opened_by_valve
+      )
+      cur = count_pressure_released(graph, inner)
+      # puts "emerged: #{cur}"
+
+      if cur > best
+        best_minute_opened_by_valve = inner
+        best = cur
+      end
     end
+
+    best_minute_opened_by_valve
+  end
+  # rubocop:enable Metrics/CyclomaticComplexity
+  # rubocop:enable Metrics/AbcSize
+  # rubocop:enable Metrics/MethodLength
+
+  def all_valves_with_flow_open?(graph, minute_opened_by_valve)
+    graph.select { |_node, valve| valve[:rate].positive? }
+         .keys
+         .all? { |node| minute_opened_by_valve[node] }
   end
 
-  def all_valves_with_flow_open?(state)
-    state.values
-         .select { |valve| valve[:rate].positive? }
-         .all? { |valve| valve[:opened_during_minute] }
-  end
-
-  def count_pressure_released(state)
-    state.values
-         .select { |valve| valve[:rate].positive? }
-         .map { |valve| (30 - (valve[:opened_during_minute] || 30)) * valve[:rate] }
+  def count_pressure_released(graph, minute_opened_by_valve)
+    graph.map { |node, valve| (MINUTES - (minute_opened_by_valve[node] || MINUTES)) * valve[:rate] }
          .sum
   end
 end
-
 if __FILE__ == $PROGRAM_NAME
-  parsed = Parser.parse('data/day_16_sample.txt')
-  pp Solution.solution(parsed)
+  graph = Parser.parse('data/day_16_sample.txt')
+  p Solution.solution(graph)
 end
