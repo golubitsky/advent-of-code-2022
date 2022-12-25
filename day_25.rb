9f3d67cdec1aef67d5c02a1e83ca6426eb38c0da
Mikhail Golubitsky <mike617@gmail.com>
2022-12-25 00:49:25 -0500

    day 25: complete part 1 (manually)

--- day_25.rb
+++ day_25.rb
@@ -4,19 +4,46 @@ module Parser
   extend self
 
   def parse(filepath)
-    File.readlines(filepath)
+    File.readlines(filepath).map(&:strip)
   end
 end
 
 module Solution
   extend self
 
+  ACTUAL_CHAR_BY_SNAFU_CHAR = {
+    '2' => 2,
+    '1' => 1,
+    '0' => 0,
+    '-' => -1,
+    '=' => -2
+  }.freeze
+
+  SNAFU_CHARS = ACTUAL_CHAR_BY_SNAFU_CHAR.keys.freeze
+
   def solution(input)
-    input
+    goal = input.sum { |snafu_number| number(snafu_number) }
+
+    synthesize_snafu(goal)
+  end
+
+  private
+
+  def number(snafu_number)
+    converted = snafu_number.reverse.chars.map.with_index do |char, index|
+      { digit: ACTUAL_CHAR_BY_SNAFU_CHAR[char], value: 5**index }
+    end
+
+    converted.map { |n| n[:digit] * n[:value] }.sum
+  end
+
+  def synthesize_snafu(n)
+    p n
+    p number('2----0=--1122=0=0021')
   end
 end
 
 if __FILE__ == $PROGRAM_NAME
-  parsed = Parser.parse('data/day_25_sample.txt')
+  parsed = Parser.parse('data/day_25.txt')
   pp Solution.solution(parsed)
 end
